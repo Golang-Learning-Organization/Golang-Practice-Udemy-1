e39c939f1bea46b222ad674ee988b9bcf1d6f36c
Gond Mohan Sukra (IT RDI SFI CSE) <MohanSukra.Gond@infineon.com>
2025-05-02 16:01:02 +0530

    Updated

--- deck.go
+++ deck.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 type deck []string
+
+func (d deck) newDeckFromFile(s string) {
+	panic("unimplemented")
+}
 
 func newDeck() deck {
 	cards := deck{}
@@ -25,4 +34,22 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	err := ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+	return err
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	return deck(strings.Split(string(bs), ","))
 }
--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 func main() {
 	cards := newDeck()
 
-	// cards.print()
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print() //
+	cards.saveToFile("my_cards")
+	cards.newDeckFromFile("my_cards")
 }
